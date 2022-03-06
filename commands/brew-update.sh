9542ebfe34589910499dce0736bc9638a61b2391
liuderchi <liuderchi@gmail.com>
2022-03-06 10:43:27 +0800

    chore(script): add custom script

---
+++ commands/brew-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Brew Update
+# @raycast.mode fullOutput
+# @raycast.packageName Raycast Scripts
+#
+# Optional parameters:
+# @raycast.icon 🍺
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Write a nice and descriptive summary about your script command here
+# @raycast.author Your name
+# @raycast.authorURL An URL for one of your social medias
+
+brew update
